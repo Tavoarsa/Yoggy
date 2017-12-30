22206e50ca80c88f324fe0f3865d5d1574ee84a3
Tavoarsa <tavoarsa@hotmail.com>
2017-12-30 10:09:27 -0600

    Index de Usuarios, controller user, layaout admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,9 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Yoggy Ema94</title>
+<title>Admin YoggyFrozen</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+
+
 </head>
-<body>
+
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+@if(\Session::has('message'))
+        @include('partials.message')
+ @endif
+<!-- Fixed navbar -->
+    
+
+
+@yield('content')
+
+
+</body>
+<!-- /Contact-Form -->
+<!-- Footer -->
+<footer class="text-center slideanim slide">
+ 
+</footer>
+<!-- js files -->
+
+
+
+
 
 </body>
 </html>
+
